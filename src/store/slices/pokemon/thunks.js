14ecd0422e9d4b7e3b5ad643e718986d720a5697
chalscc <carlescatalancalabuig@gmail.com>
2023-12-15 19:53:38 +0100

    Http Request haciendo uso de Axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,11 +1,13 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 export const getPokemons = ( page = 0 ) => {
   return async(dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    // TODO: Realizar petición http
+    const resp = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
 
-    // dispatch(setPokemons([]));
+    dispatch(setPokemons({pokemons: resp.data.results, page: page + 1}));
+
   }
 }
