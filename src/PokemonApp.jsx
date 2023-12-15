ece2bbf5982d67f2eab458c2b7ed33cd2327211c
chalscc <carlescatalancalabuig@gmail.com>
2023-12-15 15:14:00 +0100

    Thunks

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getPokemons } from './store/slices/pokemon';
 
 const PokemonApp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, [])
+
   return (
     <>
       <h1>PokemonApp</h1>
