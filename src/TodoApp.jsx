81c98139a0ffc8e7335c67f4a0a7dad26b4153a2
chalscc <carlescatalancalabuig@gmail.com>
2023-12-17 18:54:17 +0100

    Consumir el API mediante el custom hook del RTK Query

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useGetTodosQuery } from './store/apis'
 
 export const TodoApp = () => {
+
+  const { data: todos, isLoading } = useGetTodosQuery();
+
   return (
     <>
       <div>TodoApp - RTK Query</div>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading: {isLoading ? 'True' : 'False'} </h4>
       <pre>...</pre>
+
+      <ul>
+        {todos?.map(({ id, title, completed }) =>
+          <li key={id}>
+            <strong> {completed ? 'Done' : 'Pending'} </strong>
+            {title}
+          </li>
+        )}
+      </ul>
+
       <button>
         Next Todo
       </button>
