e6d2c80989c577123acdd05d6c0b8b9209150397
chalscc <carlescatalancalabuig@gmail.com>
2023-12-17 18:38:09 +0100

    RTK Query

---
+++ src/TodoApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const TodoApp = () => {
+  return (
+    <>
+      <div>TodoApp - RTK Query</div>
+      <hr />
+      <h4>isLoading...</h4>
+      <pre>...</pre>
+      <button>
+        Next Todo
+      </button>
+    </>
+  )
+}
